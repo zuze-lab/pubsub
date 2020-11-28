d0bbbc41ee63972bff12a24885b2bd21caf29db0
akmjenkins <akmjenkins@gmail.com>
2020-11-27 22:33:31 -0400

    pipe should always output undefined

--- src/operators/pipe.js
+++ src/operators/pipe.js
@@ -1,4 +1,8 @@
-export const pipe = (...fns) =>
-  fns.reduceRight((acc, fn) => fn(acc), fns[fns.length - 1]);
+export const pipe = (...fns) => {
+  const p = fns.reduceRight((acc, fn) => fn(acc), fns[fns.length - 1]);
+  return r => {
+    p(r);
+  };
+};
 
 export default () => pipe;
