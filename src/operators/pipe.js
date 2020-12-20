ee67c3ea9a6ba2427b79188797138d3992d1f589
akmjenkins <akmjenkins@gmail.com>
2020-12-19 20:09:10 -0400

    Non-sensical extra function

--- src/operators/pipe.js
+++ src/operators/pipe.js
@@ -1,8 +1,4 @@
-export const pipe = (...fns) => {
-  const p = fns.reduceRight((acc, fn) => fn(acc), fns[fns.length - 1]);
-  return r => {
-    p(r);
-  };
-};
+export const pipe = (...fns) =>
+  fns.reduceRight((acc, fn) => fn(acc), fns[fns.length - 1]);
 
 export default () => pipe;
